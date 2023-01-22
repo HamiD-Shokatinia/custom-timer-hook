e5c97604f3497ea60f7e8fe22b3c92ad4ee5b7c8
hamid Shokatinia <Hamid.shokatinia@outlook.com>
2023-01-22 19:28:09 +0330

    gh-pages config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from "react";
+import "./App.css";
+import { useTimer } from "./hooks/timer.hook";
 
 function App() {
+  const timer = useTimer(10);
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div>
+        Timer: {timer.value}
+      </div>
+      <div>
+        Is Timer done?: {timer.isDone ? "Yes" : "Not Yet"}
+      </div>
+      <button type="button" onClick={() => timer.start()}>
+        Start Timer
+      </button>
+      <button type="button" onClick={() => timer.stop()}>
+        Stop Timer
+      </button>
+      <button type="button" onClick={() => timer.reset()}>
+        Reset Timer
+      </button>
     </div>
   );
 }
